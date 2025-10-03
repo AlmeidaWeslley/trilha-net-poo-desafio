d8e518253dd36f58f8edf50b0f752c367672499d
Weslley Almeida <weslley.almeida.f@gmail.com>
2025-10-02 22:52:06 -0300

    Desafio concluido!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,64 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        private string modelo;
+        private string imei;
+        private int memoria;
+
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public string Modelo {
+            get => modelo;
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new Exception("O modelo não pode ser vazio!");
+                }
+                modelo = value;
+            }
+        }
+
+        public string Imei
+        {
+            get => imei;
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new Exception("O IMEI não pode ser vazio!");
+                }
+                imei = value;
+            }
+        }
+
+        public int Memoria
+        { 
+            get => memoria;
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Essa memoria não existe!");
+                }
+                memoria = value;
+            }
+        }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Essa é ação do Nokia!");
+Nokia nokia = new Nokia("999999999", "Modelo 1", "111111", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n------------------\n");
+
+Console.WriteLine("Essa é ação do Iphone!");
+Iphone iphone = new Iphone("87878777271", "Modelo 4", "333333333333", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
